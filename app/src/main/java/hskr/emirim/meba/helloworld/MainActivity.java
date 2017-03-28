66abdeb1b60e0824426f55daf2be91ee2dfcba5d
Hyesu Kang <hsklsj2@e-mirim.hs.kr>
2017-03-28 20:54:24 +0900

    #2 버튼 누르면 짜잔

--- app/src/main/java/hskr/emirim/meba/helloworld/MainActivity.java
+++ app/src/main/java/hskr/emirim/meba/helloworld/MainActivity.java
@@ -2,15 +2,23 @@ package hskr.emirim.meba.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnHello = (Button) findViewById(R.id.btnHello);
+        btnHello.setOnClickListener(this);
     }
 
-
-
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "지구가 멸망했따!!!!!",Toast.LENGTH_LONG).show();
+    }
 }
